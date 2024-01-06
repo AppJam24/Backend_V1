466699657f0b67abbf3d63b68cbe4a28a3d199be
anys34 <anyeseong34@gmail.com>
2024-01-06 19:48:30 +0900

    init

---
+++ src/main/java/com/anys34/azalea/AzaleaApplication.java
@@ -0,0 +1,13 @@
+package com.anys34.azalea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzaleaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzaleaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anys34/azalea/AzaleaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anys34.azalea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzaleaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
